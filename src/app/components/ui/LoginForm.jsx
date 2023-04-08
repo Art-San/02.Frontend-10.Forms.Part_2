5d97ca9a9287357b9173b6850c9df5839722b711
Aleksander <asava@sibmail.com>
2023-04-08 23:11:07 +0700

    Библиотека Yup

--- src/app/components/ui/LoginForm.jsx
+++ src/app/components/ui/LoginForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { validator } from '../../utils/validator'
+// import { validator } from '../../utils/validator'
 import CheckBoxField from '../common/form/CheckBoxField'
 import TextField from '../common/form/TextField'
+import * as yup from 'yup' // Библиотека Yup
 
 const LoginForm = () => {
     const [data, setData] = useState({ email: '', password: '', stayOn: false })
@@ -12,42 +13,75 @@ const LoginForm = () => {
             [target.name]: target.value
         }))
     }
+    // Библиотека Yup ПРИКОЛЬНАЯ
+    const validateSchema = yup.object().shape({
+        password: yup
+            .string()
+            .required('Пароль обязателен для заполнения')
+            .matches(
+                /^(?=.*[A-Z])/,
+                'Пароль должен содержать хотя бы одну заглавную букву'
+            )
+            .matches(
+                /(?=.*[0-9])/,
+                'Пароль должен содержать хотя бы одну цифру'
+            )
+            .matches(
+                /(?=.*[!@#$%^&*])/,
+                'Пароль должен содержать один из специальных символов !@#$%^&*'
+            )
+            .matches(/(?=.{8,})/, 'Пароль должен быть не менее 8 символов'),
+        email: yup
+            .string()
+            .required('Электронная почта обязательна для заполнения')
+            .matches(/^\S+@\S+\.\S+$/g, 'Email введен не некорректно') // сам уже добавил
+        // .email('Email введен не некорректно') // тут только собаку проверяет , а точку уже нет
+    })
 
-    const validatorConfig = {
-        email: {
-            isRequired: {
-                message: 'Электронная почта обязательна для заполнения'
-            },
-            isEmail: {
-                message: 'Email введен не некорректно'
-            }
-        },
-        password: {
-            isRequired: {
-                message: 'Пароль обязательна для заполнения'
-            },
-            isCapitalSymbol: {
-                message: 'Пароль должен содержать хотя бы одну заглавную букву'
-            },
-            isContainDigit: {
-                message: 'Пароль должен содержать хотя бы одну цифру'
-            },
-            min: {
-                message: 'Пароль должен быть не менее 8 символов',
-                value: 8
-            }
-        }
-    }
+    // const validatorConfig = {
+    //     email: {
+    //         isRequired: {
+    //             message: 'Электронная почта обязательна для заполнения'
+    //         },
+    //         isEmail: {
+    //             message: 'Email введен не некорректно'
+    //         }
+    //     },
+    //     password: {
+    //         isRequired: {
+    //             message: 'Пароль обязателен для заполнения'
+    //         },
+    //         isCapitalSymbol: {
+    //             message: 'Пароль должен содержать хотя бы одну заглавную букву'
+    //         },
+    //         isContainDigit: {
+    //             message: 'Пароль должен содержать хотя бы одну цифру'
+    //         },
+    //         min: {
+    //             message: 'Пароль должен быть не менее 8 символов',
+    //             value: 8
+    //         }
+    //     }
+    // }
 
     useEffect(() => {
         validate()
     }, [data])
 
+    // Библиотека Yup
     const validate = () => {
-        const errors = validator(data, validatorConfig)
-        setErrors(errors)
+        validateSchema
+            .validate(data)
+            .then(() => setErrors({})) // если положительный рез то очищаем ошибки
+            .catch((err) => setErrors({ [err.path]: err.message }))
         return Object.keys(errors).length === 0
     }
+
+    // const validate = () => {
+    //     const errors = validator(data, validatorConfig)
+    //     setErrors(errors)
+    //     return Object.keys(errors).length === 0
+    // }
 
     const isValid = Object.keys(errors).length === 0
 
